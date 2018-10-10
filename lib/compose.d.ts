cd0618c43e6820a23aac46e3182a7892207c0d3d
Fabian Trinler <fabian.trinler@blateral.com>
2018-10-10 12:15:29 +0200

    Building lib

--- lib/compose.d.ts
+++ lib/compose.d.ts
@@ -1,2 +1,7 @@
+export declare enum Dir {
+    RTL = -1,
+    LTR = 1
+}
+export declare const _compose: (dir: Dir) => (fns: Function[], data: object | any[]) => object | any[];
 declare const _default: import("./utils/curry").CurriedFunction2<Function[], object | any[], object | any[]>;
 export default _default;
---
+++ lib/pipe.d.ts
@@ -0,0 +1,2 @@
+declare const _default: import("./utils/curry").CurriedFunction2<Function[], object | any[], object | any[]>;
+export default _default;
