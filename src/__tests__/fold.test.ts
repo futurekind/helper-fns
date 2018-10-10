21473a3f3567d47ce573783319ab832f26c9e0e0
Fabian Trinler <fabian.trinler@blateral.com>
2018-10-10 11:23:00 +0200

    Adding fold functionality for objects

--- src/__tests__/fold.test.ts
+++ src/__tests__/fold.test.ts
@@ -2,12 +2,38 @@ import fold from '../fold';
 
 describe('fold', () => {
     const reducer = (acc, x) => acc + x;
-    const reduce = fold(reducer, 0);
+    const sum = fold(reducer, 0);
 
     it('reduces arrays', () => {
         const data = [1, 2, 3];
-        const result = reduce(data);
+        const result = sum(data);
 
         expect(result).toBe(6);
+    });
+
+    it('reduces objects', () => {
+        const data = {
+            some: 1,
+            foo: 2,
+            bar: 3,
+        };
+
+        const result = sum(data);
+
+        expect(result).toBe(6);
+    });
+
+    it('reduces objects II', () => {
+        const data = {
+            some: 1,
+            foo: 2,
+            bar: 3,
+        };
+
+        const reducer = (acc, val) => [...acc, val];
+
+        const result = fold(reducer, [], data);
+
+        expect(result).toEqual([1, 2, 3]);
     });
 });
--- src/fold.ts
+++ src/fold.ts
@@ -1,4 +1,5 @@
 import { curry3 } from './utils/curry';
+import isObj from './utils/isObj';
 
 const fold = (
     reducer: (acc: any, val: any, index?: number) => any,
@@ -8,6 +9,15 @@ const fold = (
     if (Array.isArray(data)) {
         return data.reduce(reducer, initialValue);
     }
+
+    if (isObj(data)) {
+        return Object.keys(data).reduce((acc, key) => {
+            const val = data[key];
+            return reducer(acc, val);
+        }, initialValue);
+    }
+
+    return data;
 };
 
 export default curry3(fold);
